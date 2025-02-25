8e0a4a7c94e9b9a89eaebbde79a97f1cee32dc53
18gen <ichihashigen@gmail.com>
2025-02-25 17:46:44 -0500

    more elements

---
+++ src/constants/logos.ts
@@ -0,0 +1,10 @@
+export const logos = [
+  {
+    src: "/logos/uw.png",
+    alt: "University of Waterloo Logo",
+  },
+  {
+    src: "/logos/wusa.png",
+    alt: "Waterloo Undergraduate Student Association Logo",
+  },
+];
